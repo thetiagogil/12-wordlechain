07ac811e7e45f1512b54e25857abb9e7c929de04
thetiagogil <talmeidagil@hotmail.com>
2024-12-04 15:38:09 +0000

    update contracts and test on contract

--- contract/contracts/MyERC20Token.sol
+++ contract/contracts/MyERC20Token.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
 
 contract MyERC20Token is ERC20 {
     constructor(uint256 initialSupply) ERC20("WordleToken", "WORD") {
--- contract/contracts/WordleGame.sol
+++ contract/contracts/WordleGame.sol
@@ -5,32 +5,21 @@ import "./MyERC20Token.sol";
 
 contract WordleGame {
     MyERC20Token public token;
-    string private wordOfTheDay;
-    address public owner;
+    string private fixedWord;
+    uint256 public constant guessFee = 1 * 10 ** 18;
 
     event GuessMade(address indexed player, string guess, bool isCorrect);
 
-    constructor(MyERC20Token _token, string memory _wordOfTheDay) {
+    constructor(MyERC20Token _token, string memory _fixedWord) {
         token = _token;
-        wordOfTheDay = _wordOfTheDay;
-        owner = msg.sender;
+        fixedWord = _fixedWord;
     }
 
     function guess(string memory userGuess) public {
         require(bytes(userGuess).length == 5, "Guess must be 5 letters long");
-        require(token.transferFrom(msg.sender, address(this), 1 * 10 ** 18), "Token transfer failed"); // 1 WORD per guess
+        require(token.transferFrom(msg.sender, address(this), guessFee), "Token transfer failed");
 
-        bool isCorrect = keccak256(abi.encodePacked(userGuess)) == keccak256(abi.encodePacked(wordOfTheDay));
+        bool isCorrect = keccak256(abi.encodePacked(userGuess)) == keccak256(abi.encodePacked(fixedWord));
         emit GuessMade(msg.sender, userGuess, isCorrect);
-
-        if (isCorrect) {
-            // Reward logic can go here, e.g., transferring tokens back to the user as a reward
-            token.transfer(msg.sender, 5 * 10 ** 18); // Example reward for correct guess
-        }
-    }
-
-    function updateWordOfTheDay(string memory newWord) public {
-        require(msg.sender == owner, "Only the owner can update the word");
-        wordOfTheDay = newWord;
     }
 }
