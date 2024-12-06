145d148466f81d481cf837e360b52ec1135e5488
thetiagogil <talmeidagil@hotmail.com>
2024-12-06 17:53:03 +0000

    change contracts, script and tests

--- contract/contracts/WordleGame.sol
+++ contract/contracts/WordleGame.sol
@@ -1,25 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./MyERC20Token.sol";
+import "./WordleToken.sol";
 
 contract WordleGame {
-    MyERC20Token public token;
-    string private fixedWord;
-    uint256 public constant guessFee = 1 * 10 ** 18;
+    WordleToken public token;
+    string private constant FIXED_WORD = "APPLE";
+    uint256 public constant GUESS_FEE = 10 * 10 ** 18;
 
     event GuessMade(address indexed player, string guess, bool isCorrect);
 
-    constructor(MyERC20Token _token, string memory _fixedWord) {
+    constructor(WordleToken _token) {
         token = _token;
-        fixedWord = _fixedWord;
     }
 
     function guess(string memory userGuess) public {
         require(bytes(userGuess).length == 5, "Guess must be 5 letters long");
-        require(token.transferFrom(msg.sender, address(this), guessFee), "Token transfer failed");
+        require(token.transferFrom(msg.sender, address(this), GUESS_FEE), "Token transfer failed");
 
-        bool isCorrect = keccak256(abi.encodePacked(userGuess)) == keccak256(abi.encodePacked(fixedWord));
+        bool isCorrect = keccak256(abi.encodePacked(userGuess)) == keccak256(abi.encodePacked(FIXED_WORD));
         emit GuessMade(msg.sender, userGuess, isCorrect);
     }
 }
--- contract/contracts/WordleToken.sol
+++ contract/contracts/WordleToken.sol
@@ -3,8 +3,7 @@ pragma solidity ^0.8.0;
 
 import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
-
-contract MyERC20Token is ERC20 {
+contract WordleToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("WordleToken", "WORD") {
         _mint(msg.sender, initialSupply);
     }
