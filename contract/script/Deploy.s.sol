09ad3e552535bd462856affd2d3747557bfe8b83
thetiagogil <talmeidagil@hotmail.com>
2024-12-10 15:47:39 +0000

    fix wordle game contract logic and add function to transfer tokens

--- contract/script/Deploy.s.sol
+++ contract/script/Deploy.s.sol
@@ -7,13 +7,24 @@ import "../contracts/WordleGame.sol";
 
 contract Deploy is Script {
     function run() external {
+        // Load environment variables
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address playerAddress = vm.envAddress("PLAYER_ADDRESS");
+
+        // Start broadcasting transactions
         vm.startBroadcast(deployerPrivateKey);
 
+        // Deploy the WordleToken contract
         WordleToken token = new WordleToken(1000 * 10 ** 18);
         console.log("WordleToken deployed at:", address(token));
-        WordleGame game = new WordleGame(token);
+
+        // Deploy the WordleGame contract
+        WordleGame game = new WordleGame(address(token));
         console.log("WordleGame deployed at:", address(game));
+        
+        // Transfer tokens to the user's address
+        token.transfer(playerAddress, 1000 * 10 ** 18);
+        console.log("1000 tokens transferred to user address:", playerAddress);
 
         vm.stopBroadcast();
     }
