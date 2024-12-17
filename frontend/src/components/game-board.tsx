f2ed216d7e4d25d7f3620f34682eeace653d5d4d
thetiagogil <talmeidagil@hotmail.com>
2024-12-17 11:37:32 +0000

    remove token use effect

--- frontend/src/components/game-board.tsx
+++ frontend/src/components/game-board.tsx
@@ -1,63 +1,127 @@
-import { useState } from "react";
+import { Box, Button, Grid, Stack } from "@mui/joy";
+import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
-import { useReadContract, useWaitForTransactionReceipt, useWriteContract } from "wagmi";
-import { WordleGameABI } from "../abis/WordleGame.abi";
-import { WORDLE_GAME_ADDRESS } from "../config/constants";
+import { useGameContract } from "../hooks/useGameContract";
+import { useTokenContract } from "../hooks/useTokenContract";
 
-type UseGameContractProps = {
-  guess: string;
-};
+const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
-export const useGameContract = ({ guess }: UseGameContractProps) => {
+export const GameBoard = () => {
   // States
-  const [hash, setHash] = useState<`0x${string}` | undefined>(undefined);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [guess, setGuess] = useState<string>("");
 
   // Hooks
-  const { writeContractAsync } = useWriteContract();
+  const { handleApproveTokens, handleCheckAllowance, allowance, isLoading: isLoadingToken } = useTokenContract();
+  const { handleSubmitGuess, hasWaitedForGuess, isGuessCorrect, isLoading: isLoadingGame } = useGameContract({ guess });
 
-  // Handle Submit Guess Button
-  const handleSubmitGuess = async (allowance: number) => {
-    setIsLoading(true);
-    try {
-      if (allowance > 0) {
-        const response = await writeContractAsync({
-          address: WORDLE_GAME_ADDRESS,
-          abi: WordleGameABI,
-          functionName: "guess",
-          args: [guess]
-        });
-        setHash(response);
+  // Game Logic
+  const handleLetterClick = (letter: string) => {
+    if (guess.length < 5) setGuess(prev => prev + letter);
+  };
+
+  const handleDelete = () => {
+    if (guess.length > 0) setGuess(prev => prev.slice(0, -1));
+  };
+
+  // Use Effects
+  useEffect(() => {
+    if (hasWaitedForGuess && isLoadingGame === false) {
+      if (isGuessCorrect) {
+        toast.success(`Your guess was correct!!`, { closeOnClick: true });
       } else {
-        toast.error("You need allowance to play the game.", { closeOnClick: true });
+        toast.error(`Your guess was incorrect...`, { closeOnClick: true });
+        setGuess("");
       }
-    } catch (err: any) {
-      toast.error("Failed to submit guess. Please try again.", { closeOnClick: true });
-      console.error(err);
-    } finally {
-      setIsLoading(false);
     }
-  };
+  }, [hasWaitedForGuess, isLoadingGame]);
 
-  // Handle Wait For Transaction Receipt
-  const { data: hasWaitedForGuess } = useWaitForTransactionReceipt({
-    hash: hash
-  });
+  useEffect(() => {
+    if (hasWaitedForGuess) {
+      console.log("Has waited?: " + hasWaitedForGuess);
+      console.log("The guess was: " + guess);
+      console.log("Was it correct?: " + hasWaitedForGuess);
+    }
+  }, [hasWaitedForGuess]);
 
-  // Get Guess Result
-  const guessObj = useReadContract({
-    abi: WordleGameABI,
-    address: WORDLE_GAME_ADDRESS,
-    functionName: "guesses",
-    args: [guess]
-  });
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+      {/* Submit Approve Tokens Button */}
+      <Stack sx={{ flexDirection: "row", gap: 1 }}>
+        <Button
+          size="lg"
+          fullWidth
+          onClick={handleApproveTokens}
+          color="success"
+          loading={isLoadingToken || isLoadingGame}
+          disabled={allowance > 0}
+        >
+          Approve Tokens
+        </Button>
+        <Button
+          size="lg"
+          fullWidth
+          onClick={handleCheckAllowance}
+          color="neutral"
+          loading={isLoadingToken || isLoadingGame}
+        >
+          Check Allowance
+        </Button>
+      </Stack>
 
-  const isGuessCorrect = guessObj?.data;
+      {/* Word */}
+      <Grid container sx={{ justifyContent: "space-between" }}>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <Grid
+            component={Stack}
+            key={index}
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+              border: "1px solid",
+              height: { xs: 65, sm: 75 },
+              width: { xs: 65, sm: 75 },
+              fontSize: 32,
+              fontWeight: "bold"
+            }}
+          >
+            {guess[index]}
+          </Grid>
+        ))}
+      </Grid>
 
-  return {
-    handleSubmitGuess,
-    hasWaitedForGuess,
-    isGuessCorrect,
-    isLoading
-  };
+      {/* Letters + Delete Button + Submit Guess Button */}
+      <Grid container spacing={0.5} columns={6} sx={{ justifyContent: "center" }}>
+        {ALPHABET.map(letter => (
+          <Grid xs={1} key={letter}>
+            <Button
+              size="lg"
+              fullWidth
+              onClick={() => handleLetterClick(letter)}
+              color="neutral"
+              disabled={isLoadingGame}
+            >
+              {letter}
+            </Button>
+          </Grid>
+        ))}
+        <Grid xs={2}>
+          <Button size="lg" fullWidth onClick={handleDelete} color="neutral" disabled={isLoadingGame}>
+            Delete
+          </Button>
+        </Grid>
+        <Grid xs={2}>
+          <Button
+            size="lg"
+            fullWidth
+            onClick={() => handleSubmitGuess(allowance)}
+            color="success"
+            disabled={guess.length < 5}
+            loading={isLoadingGame}
+          >
+            Submit
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
