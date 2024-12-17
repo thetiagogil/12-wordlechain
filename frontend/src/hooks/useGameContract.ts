d0ec71109810fdfd0e2306a85e8b2daeb229f8f3
thetiagogil <talmeidagil@hotmail.com>
2024-12-17 11:45:13 +0000

    update frontend hook to match new contract functions

--- frontend/src/hooks/useGameContract.ts
+++ frontend/src/hooks/useGameContract.ts
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
-import { useReadContract, useWaitForTransactionReceipt, useWriteContract } from "wagmi";
+import { useAccount, useReadContract, useWaitForTransactionReceipt, useWriteContract } from "wagmi";
 import { WordleGameABI } from "../abis/WordleGame.abi";
 import { WORDLE_GAME_ADDRESS } from "../config/constants";
 
@@ -14,22 +14,49 @@ export const useGameContract = ({ guess }: UseGameContractProps) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   // Hooks
+  const { address: userAddress } = useAccount();
   const { writeContractAsync } = useWriteContract();
+
+  // Read User Guesses
+  const getUserGuesses =
+    useReadContract({
+      abi: WordleGameABI,
+      address: WORDLE_GAME_ADDRESS,
+      functionName: "getUserGuesses",
+      args: [userAddress]
+    }).data || [];
+
+  // Read Has User Guessed Correctly
+  const hasUserGuessedCorrectly =
+    useReadContract({
+      abi: WordleGameABI,
+      address: WORDLE_GAME_ADDRESS,
+      functionName: "hasUserGuessedCorrectly",
+      args: [userAddress]
+    }).data || false;
 
   // Handle Submit Guess Button
   const handleSubmitGuess = async (allowance: number) => {
     setIsLoading(true);
     try {
-      if (allowance > 0) {
-        const response = await writeContractAsync({
-          address: WORDLE_GAME_ADDRESS,
-          abi: WordleGameABI,
-          functionName: "guess",
-          args: [guess]
-        });
-        setHash(response);
-      } else {
-        toast.error("You need allowance to play the game.", { closeOnClick: true });
+      switch (true) {
+        case allowance <= 0:
+          toast.error("You need allowance to play the game.", { closeOnClick: true });
+          break;
+        case hasUserGuessedCorrectly:
+          toast.error("You have already guessed correctly!", { closeOnClick: true });
+          break;
+        case Array.isArray(getUserGuesses) && getUserGuesses.length >= 5:
+          toast.error("You already exceeded the limit play tries for today!", { closeOnClick: true });
+          break;
+        default:
+          const response = await writeContractAsync({
+            address: WORDLE_GAME_ADDRESS,
+            abi: WordleGameABI,
+            functionName: "guess",
+            args: [guess]
+          });
+          setHash(response);
       }
     } catch (err: any) {
       toast.error("Failed to submit guess. Please try again.", { closeOnClick: true });
@@ -40,19 +67,17 @@ export const useGameContract = ({ guess }: UseGameContractProps) => {
   };
 
   // Handle Wait For Transaction Receipt
-  const { data: hasWaitedForGuess } = useWaitForTransactionReceipt({
+  const { isSuccess: hasWaitedForGuess } = useWaitForTransactionReceipt({
     hash: hash
   });
 
-  // Get Guess Result
-  const guessObj = useReadContract({
+  // Read Guess Result
+  const isGuessCorrect = useReadContract({
     abi: WordleGameABI,
     address: WORDLE_GAME_ADDRESS,
     functionName: "guesses",
     args: [guess]
-  });
-
-  const isGuessCorrect = guessObj?.data;
+  }).data;
 
   return {
     handleSubmitGuess,
