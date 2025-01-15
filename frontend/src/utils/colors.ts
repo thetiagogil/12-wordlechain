eb0cd7fafdb92f71721de56e4ef9367365f135af
Tiago Gil <150550702+thetiagogil@users.noreply.github.com>
2025-01-15 01:03:38 +0000

    Add keyboard colors (#26)
    This PR adds background colors to the keyboard after a player has made a
    guess. The colors of the keyboard match the colors used in the guesses
    row.
    
    
    ![image](https://github.com/user-attachments/assets/c1368409-ff65-450b-985d-8dc4b3182192)

---
+++ frontend/src/utils/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  black: "#121213",
+  green: "#538D4E",
+  yellow: "#B59F3B",
+  grey: "#222529"
+};
+
+export const LETTER_BG_COLORS: { [key: number]: string } = {
+  2: colors.green,
+  1: colors.yellow,
+  0: colors.grey
+};
